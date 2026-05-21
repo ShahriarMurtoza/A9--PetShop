a91daf77c007cf7c5be1896772a7a7092922e6b0
ShahriarMurtoza <shahriarMurtoza.dev@gmail.com>
2026-05-21 23:14:04 +0600

    little fix

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -43,12 +43,7 @@ const Navbar = () => {
           <li>
             <Link href={"/"}>Home</Link>
           </li>
-          <li>
-            <Link href={"/all-courses"}>All Courses</Link>
-          </li>
-          <li>
-            <Link href={"/profile"}>Profile</Link>
-          </li>
+
 
           {/* Added Pages */}
           <li>
@@ -123,13 +118,6 @@ const Navbar = () => {
               <Link href={"/"}>Home</Link>
             </li>
 
-            <li>
-              <Link href={"/all-courses"}>All Courses</Link>
-            </li>
-
-            <li>
-              <Link href={"/profile"}>Profile</Link>
-            </li>
 
             {/* Added Pages */}
             <li>
